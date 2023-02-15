99b5e8b7d9aec5c08fa52d0ee1c9137283fd7a57
Angelina <an_ivanova@hotmail.com>
2023-02-15 16:03:57 +0300

    New App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
+import Nav from "./nav/Nav";
+import Main from "./main/Main";
+import Skills from "./skills/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Header/>
+     <Main/>
+     <Skills/>
+   </div>
+
   );
 }
 
